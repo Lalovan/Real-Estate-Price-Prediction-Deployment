cb792421cb87762bbb25bece20065b3a39739b6d
Lalovan <anna.ivailova@gmail.com>
2025-12-03 14:58:30 +0100

    Added typing.Literal to show options in categorical features in the API;

--- FastAPI.py
+++ FastAPI.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from typing import Union
+from typing import Union, Literal
 from pydantic import BaseModel
 import joblib
 import pandas as pd
@@ -7,20 +7,36 @@ import pandas as pd
 app= FastAPI()
 
 class Features(BaseModel):
-    total_area_sqm: float
-    cadastral_income: float
-    primary_energy_consumption_sqm:float
-    nbr_bedrooms: int
-    nbr_frontages: int
-    subproperty_type:str
-    province:str
-    fl_terrace: bool
-    fl_garden: bool
-    fl_swimming_pool: bool
-    fl_furnished: bool
-    epc: str
-    equipped_kitchen:str
-    heating_type:str
+    total_area_sqm: float #
+    cadastral_income: float #
+    primary_energy_consumption_sqm:float #
+    nbr_bedrooms: int #
+    nbr_frontages: int # max 4
+    subproperty_type: Literal[
+        "APARTMENT", "HOUSE", "DUPLEX", "VILLA", "EXCEPTIONAL_PROPERTY",
+        "FLAT_STUDIO", "GROUND_FLOOR", "PENTHOUSE", "FARMHOUSE",
+        "APARTMENT_BLOCK", "COUNTRY_COTTAGE", "TOWN_HOUSE", "SERVICE_FLAT",
+        "MANSION", "MIXED_USE_BUILDING", "MANOR_HOUSE", "LOFT",
+        "BUNGALOW", "KOT", "CASTLE", "CHALET", "TRIPLEX", "OTHER_PROPERTY"
+    ]
+    province: Literal[
+        "Antwerp", "East Flanders", "Brussels", "Walloon Brabant",
+        "Flemish Brabant", "Liège", "West Flanders", "Hainaut",
+        "Luxembourg", "Limburg", "Namur"
+    ]
+    fl_terrace: bool #
+    fl_garden: bool #
+    fl_swimming_pool: bool #
+    fl_furnished: bool #
+    epc: Literal["excellent","good","poor","bad","unknown"]
+    
+    equipped_kitchen: Literal[
+        "UNKNOWN", "INSTALLED", "SEMI-EQUIPPED", "HYPER-EQUIPPED"
+    ]
+
+    heating_type: Literal[
+        "GAS", "ELECTRIC", "HEAT_PUMP", "OIL", "OTHER"
+    ]
 
 model_path = "model.pkl"
 pipeline = joblib.load(model_path)
